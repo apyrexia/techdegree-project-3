9886d0bab1584b076b4c200631dbdd8f498bfe92
Ethan W <elwaterson@gmail.com>
2020-06-26 19:01:54 -0400

    Filters available color options by T-shirt theme

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 const nameField = document.getElementById('name');
 const otherTitle = document.getElementById('other-title');
 const titleSelect = document.getElementById('title');
+const colorSelect = document.getElementById('color');
+const colorOptions = document.querySelector("#color").children;
+const shirtDesign = document.getElementById('design');
 
 // Focus on the name field on page load
 nameField.focus();
@@ -15,5 +18,43 @@ titleSelect.addEventListener("change", e => {
     otherTitle.style.display = "";
   } else if (e.target.value !== "other") {
     otherTitle.style.display = "none";
+  }
+});
+
+// Filters available color options by t-shirt theme
+const colorDefault = document.createElement('option');
+  colorSelect.appendChild(colorDefault);
+  colorDefault.selected = "selected";
+  colorDefault.textContent = "Please select a T-shirt theme";
+
+for (i = 0; i < colorOptions.length; i++) {
+colorOptions[i].style.display = "none";
+};
+
+shirtDesign.addEventListener("change", e => { 
+  colorDefault.selected = "selected";
+  if (e.target.value === "js puns") {
+    colorDefault.textContent = "Please select a Color";
+    for (i = 0; i < colorOptions.length; i++) {
+      if (i < 3) {
+        colorOptions[i].style.display = "";
+      } else if (i > 2) {
+        colorOptions[i].style.display = "none";
+      }
+    };
+  } else if (e.target.value === "heart js") {
+    colorDefault.textContent = "Please select a Color";
+    for (i = 0; i < colorOptions.length; i++) {
+      if (i > 2 && i < 6) {
+        colorOptions[i].style.display = "";
+      } else if (i < 3) {
+        colorOptions[i].style.display = "none";
+      }
+    };
+  } else if (e.target.value === "Select Theme") {
+    colorDefault.textContent = "Please select a T-shirt theme";
+    for (i = 0; i < colorOptions.length; i++) {
+      colorOptions[i].style.display = "none";
+      };
   }
 });
